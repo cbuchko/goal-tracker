7c3e41e66d38757d5d1c45b15f952f6c284716db
Connor <connorbuchko2010@yahoo.ca>
2020-01-23 16:56:24 -0800

    Fixed edit button layering

--- js/script.js
+++ js/script.js
@@ -197,7 +197,7 @@ function loadGoalManager() {
     for (i = 0; i < acc.length; i++) {
         acc[i].addEventListener("click", function () {
             this.classList.toggle("active");
-            var panel = this.nextElementSibling;
+            var panel = this.parentNode.nextElementSibling;
             if (panel.style.maxHeight) {
                 panel.style.maxHeight = null;
             } else {
