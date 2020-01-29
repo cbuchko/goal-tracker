29637b18548a69481396bb7c43c19a07de3c8431
Kahvi Patel <iamkahvi@gmail.com>
2020-01-28 19:13:44 -0800

    KP: adding npm stuff, goal component

--- goal_manager.js
+++ goal_manager.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -9,72 +9,93 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var GoalManager = function (_React$Component) {
-    _inherits(GoalManager, _React$Component);
+  _inherits(GoalManager, _React$Component);
 
-    function GoalManager(Goal) {
-        _classCallCheck(this, GoalManager);
+  function GoalManager() {
+    _classCallCheck(this, GoalManager);
 
-        var _this = _possibleConstructorReturn(this, (GoalManager.__proto__ || Object.getPrototypeOf(GoalManager)).call(this, Goal));
+    return _possibleConstructorReturn(this, (GoalManager.__proto__ || Object.getPrototypeOf(GoalManager)).call(this));
+  }
 
-        _this.state = { liked: false };
-        return _this;
+  // Function to create goal list element
+
+
+  _createClass(GoalManager, [{
+    key: "buildGoalManager",
+    value: function buildGoalManager(goal) {
+      return React.createElement(
+        "li",
+        { id: "goal-manager-" + goal.name, key: goal.name },
+        React.createElement(
+          "div",
+          { className: "accordion-header" },
+          React.createElement(
+            "button",
+            { className: "accordion" },
+            React.createElement(
+              "div",
+              { className: "goal-manager-name" },
+              React.createElement("input", { disabled: "", defaultValue: goal.name })
+            ),
+            React.createElement(
+              "div",
+              { className: "goal-manager-description" },
+              React.createElement("input", { disabled: "", defaultValue: goal.description })
+            ),
+            React.createElement(
+              "div",
+              { className: "goal-manager-deadline" },
+              React.createElement("input", {
+                type: "date",
+                disabled: "",
+                defaultValue: goal.deadline
+              })
+            )
+          ),
+          React.createElement(
+            "div",
+            { className: "goal-manager-widgets", style: { justifySelf: "end" } },
+            React.createElement("img", {
+              className: "edit-widget",
+              src: "../images/edit.svg",
+              alt: "edit"
+              // onClick={`enableGoalEdit("${goal.name}")`}
+            }),
+            React.createElement("img", {
+              src: "../images/trashcan.svg",
+              alt: "trashcan"
+              // onClick={`'deleteGoal("${goal.name}")'`}
+            })
+          )
+        ),
+        React.createElement(
+          "div",
+          { className: "dropdown-panel" },
+          React.createElement(
+            "p",
+            null,
+            "hello"
+          )
+        )
+      );
     }
+  }, {
+    key: "render",
+    value: function render() {
+      var _this2 = this;
 
-    _createClass(GoalManager, [{
-        key: "render",
-        value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        JSON.parse(window.localStorage.getItem("master_list")).map(function (goal) {
+          return _this2.buildGoalManager(goal);
+        })
+      );
+    }
+  }]);
 
-            return React.createElement(
-                "li",
-                { id: "goal-manager-my-goal" },
-                React.createElement(
-                    "div",
-                    { "class": "accordion-header" },
-                    React.createElement(
-                        "button",
-                        { "class": "accordion" },
-                        React.createElement(
-                            "div",
-                            { "class": "goal-manager-name" },
-                            React.createElement("input", { id: "goal-manager-my-goal", type: "text", value: "My Goal",
-                                disabled: "true" })
-                        ),
-                        React.createElement(
-                            "div",
-                            { "class": "goal-manager-description" },
-                            React.createElement("input", { id: "goal-manager-my-description",
-                                value: "My description", disabled: "true" })
-                        ),
-                        React.createElement(
-                            "div",
-                            { "class": "goal-manager-deadline" },
-                            React.createElement("input", { id: "goal-manager-my-deadline", type: "date",
-                                value: "1999-07-31", disabled: "true" })
-                        )
-                    ),
-                    React.createElement(
-                        "div",
-                        { "class": "goal-manager-widgets", style: "justify-self: end;" },
-                        React.createElement("img", { id: "goal-manager-edit-my-goal", src: "../images/edit.svg", alt: "edit goal",
-                            onClick: "enableGoalEdit()", role: "button" }),
-                        React.createElement("img", { src: "../images/trashcan.svg", alt: "trashcan", onClick: "deleteGoal()", role: "button" })
-                    )
-                ),
-                React.createElement(
-                    "div",
-                    { "class": "dropdown-panel" },
-                    React.createElement(
-                        "p",
-                        null,
-                        " hello "
-                    )
-                )
-            );
-        }
-    }]);
-
-    return GoalManager;
+  return GoalManager;
 }(React.Component);
 
-var domContainer = document.querySelector('#goal-manager-unordered-list');
+var domContainer = document.querySelector("#goal-manager-unordered-list");
 ReactDOM.render(React.createElement(GoalManager, null), domContainer);
--- src/goal_manager.js
+++ src/goal_manager.js
@@ -1,37 +1,61 @@
-'use strict';
+"use strict";
 
 class GoalManager extends React.Component {
-  constructor(Goal) {
-    super(Goal);
-    this.state = { liked: false };
+  constructor() {
+    super();
+  }
+
+  // Function to create goal list element
+  buildGoalManager(goal) {
+    return (
+      <li id={`goal-manager-${goal.name}`} key={goal.name}>
+        <div className="accordion-header">
+          <button className="accordion">
+            <div className="goal-manager-name">
+              <input disabled="" defaultValue={goal.name}></input>
+            </div>
+            <div className="goal-manager-description">
+              <input disabled="" defaultValue={goal.description}></input>
+            </div>
+            <div className="goal-manager-deadline">
+              <input
+                type="date"
+                disabled=""
+                defaultValue={goal.deadline}
+              ></input>
+            </div>
+          </button>
+          <div className="goal-manager-widgets" style={{ justifySelf: "end" }}>
+            <img
+              className="edit-widget"
+              src="../images/edit.svg"
+              alt="edit"
+              // onClick={`enableGoalEdit("${goal.name}")`}
+            ></img>
+            <img
+              src="../images/trashcan.svg"
+              alt="trashcan"
+              // onClick={`'deleteGoal("${goal.name}")'`}
+            ></img>
+          </div>
+        </div>
+        <div className="dropdown-panel">
+          <p>hello</p>
+        </div>
+      </li>
+    );
   }
 
   render() {
-
     return (
-        <li id="goal-manager-my-goal">
-            <div class="accordion-header">
-                <button class="accordion">
-                    <div class="goal-manager-name"><input id="goal-manager-my-goal" type="text" value="My Goal"
-                            disabled="true"></input></div>
-                    <div class="goal-manager-description"><input id="goal-manager-my-description"
-                            value="My description" disabled="true"></input></div>
-                    <div class="goal-manager-deadline"><input id="goal-manager-my-deadline" type="date"
-                            value="1999-07-31" disabled="true"></input></div>
-                </button>
-                <div class="goal-manager-widgets" style="justify-self: end;">
-                    <img id="goal-manager-edit-my-goal" src="../images/edit.svg" alt="edit goal"
-                        onClick="enableGoalEdit()" role="button"></img>
-                    <img src="../images/trashcan.svg" alt="trashcan" onClick="deleteGoal()" role="button"></img>
-                </div>
-            </div>             
-            <div class="dropdown-panel">
-                <p> hello </p>
-            </div>
-        </li>
+      <div>
+        {JSON.parse(window.localStorage.getItem("master_list")).map(goal => {
+          return this.buildGoalManager(goal);
+        })}
+      </div>
     );
   }
 }
 
-let domContainer = document.querySelector('#goal-manager-unordered-list');
+let domContainer = document.querySelector("#goal-manager-unordered-list");
 ReactDOM.render(<GoalManager />, domContainer);
